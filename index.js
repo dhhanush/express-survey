7c102a9310006e25cd02747a66bab7609e0b139d
Dhanush <dhanush.dev17@gmail.com>
2021-06-27 08:23:18 +0530

    google oauth setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+
+const keys = require('./config/keys');
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.sessionSecret],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(
+  keys.mongoDBURI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+  }
+);
+
+require('./models/User');
+require('./services/passport');
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
---
+++ services/passport.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id).then((user) => {
+    cb(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            return done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then((user) => done(null, user));
+          }
+        })
+        .catch((err) => console.log(err));
+      // const newUser = new User({ googleId: profile.id });
+      // newUser
+      //   .save()
+      //   .then((user) => {
+      //     return done(null, user);
+      //   })
+      //   .catch((err) => console.log(err));
+    }
+  )
+);
