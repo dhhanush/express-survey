d449571e4ac802406d3328c1ace291133051c3d5
Dhanush <dhanush.dev17@gmail.com>
2021-07-12 23:47:26 +0530

    fix style issue

--- client/src/components/Thanks.jsx
+++ client/src/components/Thanks.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 export default function Thanks() {
   return (
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card">
-          <div class="card-image">
-            <span class="card-title">Thank You</span>
-          </div>
-          <div class="card-content">
-            <p>Thank You for taking part in the survey</p>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <span class="card-title">Thank You</span>
+            </div>
+            <div class="card-content">
+              <p>Thank You for taking part in the survey</p>
+            </div>
           </div>
         </div>
       </div>
